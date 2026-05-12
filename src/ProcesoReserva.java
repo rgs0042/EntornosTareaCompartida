bea30c65e68514268844802c6fa0a9e90aa058f2
cmv0023 <cmv0023@alu.medac.es>
2026-05-12 20:26:03 +0200

    Código Comentado

--- src/ProcesoReserva.java
+++ src/ProcesoReserva.java
@@ -14,13 +14,24 @@ public class ProcesoReserva {
      * @return true si se pudo reservar, false si ya estaba ocupada.
      */ 
 
+          // Método que devuelve true si la reserva se cumplió o false si falló
           public boolean reservar(int numeroHabitacion) {
+
+            // Bucle for-each, ecorre uno por uno los objetos 'Habitacion' en la lista 'habitaciones'
             for (Habitacion h : habitaciones) {
-            if (h.getNumero() == numeroHabitacion && !h.isOcupada()) {
+            
+                // Comprueba que el número de la habitación actual sea el que buscamos y que la habitación no esté ocupada 
+                if (h.getNumero() == numeroHabitacion && !h.isOcupada()) {
+               
+                // Si ambas condiciones se cumplen, marcamos la habitación como ocupada
                 h.setOcupada(true);
+                
+                // Salimos devolviendo true
                 return true;
             }
         }
+         
+        // Si el bucle termina y no se encontró la habitación o estaba ocupada, devuelve false
         return false;
     }
 
