2a86e885c53391e2b5fa77fedd888c1d426a71ae
jrc007003 <jrc0070@alu.medac.es>
2026-05-12 20:03:24 +0200

    Primen Cambio Main

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * Clase principal que lanza la aplicación de consola.
+ * @author Joseka
+ */
+public class Main {
+    public static void main(String[] args) {
+        GestorReservas gestor = new GestorReservas();
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+
+        System.out.println("--- BIENVENIDO A JAVA HOTEL ---");
+    }
+}
