a8435f9494251a567133413e4bf528c69abd0261
cmv0023 <cmv0023@alu.medac.es>
2026-05-12 19:31:21 +0200

    1 commit

--- src/App.java
+++ src/App.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 
 /**
  * Clase principal que lanza la aplicación de consola.
- * @author 
+ * @author Carlos Javier Millán Villarejo
  */
 public class App {
     public static void main(String[] args) throws Exception {
         
         
 
-        System.out.println("--- BIENVENIDO A JAVA HOTEL ---");
-
-        
-        
-        
-        System.out.println("Saliendo del sistema...");
+       System.out.println("hola");
     }
 }
