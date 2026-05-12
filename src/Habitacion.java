7469ba11639a39f8051a782afb6a4fa601f900a7
Antonio250705 <agc0147@alu.medac.es>
2026-05-12 19:20:12 +0200

    primer commit de la clase Habitacion

---
+++ src/Habitacion.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+/**
+ * Clase principal que lanza la aplicación de consola.
+ * @author Antonio García
+ */
+
+    public class Habitacion {
+        private int numero;
+        private String tipo; // Simple, Doble, Suite
+        private double precioBase;
+        private boolean estaOcupada;
+        
+        
+        
+       
+}
