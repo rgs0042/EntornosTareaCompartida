9885bfee9f71cdf0bfe1d82e4b6948b7f1461189
jrc007003 <jrc0070@alu.medac.es>
2026-05-12 20:04:38 +0200

    Segundo Cambio de Código

--- src/Main.java
+++ src/Main.java
@@ -11,5 +11,24 @@ public class Main {
         int opcion;
 
         System.out.println("--- BIENVENIDO A JAVA HOTEL ---");
+        do {
+            System.out.println("\n1. Ver disponibles\n2. Reservar\n3. Salir");
+            System.out.print("Seleccione opción: ");
+            opcion = sc.nextInt();
+
+            if (opcion == 1) {
+                System.out.println("Habitaciones libres:");
+                gestor.obtenerDisponibles().forEach(h -> 
+                    System.out.println("Nº " + h.getNumero() + " - " + h.getPrecioBase() + "€"));
+            } else if (opcion == 2) {
+                System.out.print("Introduce número de habitación: ");
+                int num = sc.nextInt();
+                if (gestor.reservar(num)) {
+                    System.out.println("¡Reserva confirmada!");
+                } else {
+                    System.out.println("Error: Habitación no disponible.");
+                }
+            }
+        } while (opcion != 3);
     }
 }
