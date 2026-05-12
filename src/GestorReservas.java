0b66a6f6437692e2bbb8e3c8dcc12686efa4db49
rgs0042 <rgs0042@alu.medac.es>
2026-05-12 19:51:39 +0200

    añadiendo cosas simples

--- src/GestorReservas.java
+++ src/GestorReservas.java
@@ -7,7 +7,14 @@ import java.util.List;
  * @author Rafa
  */
 public class GestorReservas {
+    private List<Habitacion> habitaciones;
+
+    public GestorReservas() {
+        habitaciones = new ArrayList<>();
+        // Datos de prueba
+        habitaciones.add(new Habitacion(101, "Simple", 50.0));
+        habitaciones.add(new Habitacion(102, "Doble", 80.0));
+        habitaciones.add(new Habitacion(201, "Suite", 150.0));
+    }
 
 }
-
-
